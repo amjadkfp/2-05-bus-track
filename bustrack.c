d65d6fc42bf8489b7a3e7c9bef500163c2a7e591
amjadkfp <103019826+amjadkfp@users.noreply.github.com>
2022-06-02 17:09:43 +0530

    02/05 bus track code
    hfhjk hhygf gh vu

---
+++ bustrack.c
@@ -0,0 +1,157 @@
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
+#include <Wire.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClientSecure.h>
+#include <ArduinoJson.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+StaticJsonDocument<196> doc;
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, & Wire, -1);
+TinyGPSPlus gps; // The TinyGPS++ object
+SoftwareSerial ss(3, 0); // The serial connection to the GPS device
+const char * ssid = "AMJAD";
+const char * password = "123456789";
+const char* serverName = "http://548ebd.deta.dev/location/new"; // post
+String output;
+float latitude, longitude;
+String lat_str, lng_str;
+String latarray[20];
+String lngarray[20];
+unsigned int i = 0;
+unsigned long Interval = 13000;
+unsigned long previousTime = 0;
+
+void displayPrint(String text, unsigned int line = 1) {
+  display.setCursor(0, line * 10);
+  display.println(text);
+  display.display();
+}
+
+void setup() {
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;);
+  }
+  delay(2000);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+
+  Serial.begin(115200);
+  ss.begin(9600);
+  delay(2000); // Pause for 2 seconds
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    display.clearDisplay();
+    displayPrint("Connecting to WIFI...", 2);
+    displayPrint(ssid, 3);
+    displayPrint("OXOX", 4);
+    delay(500);
+    display.clearDisplay();
+    displayPrint("Connecting to WIFI...", 2);
+    displayPrint(ssid, 3);
+    displayPrint("XOXO", 4);
+    delay(500);
+  }
+  display.clearDisplay();
+  displayPrint("WiFi Connected!", 2);
+}
+
+void checkandreadGPS() {
+  unsigned long currentTime = millis();
+  if (gps.encode(ss.read())) {
+    if (currentTime - previousTime >= Interval) {
+      if (gps.location.isValid()) {
+        //Serial.print("Getting Data");
+        latitude = gps.location.lat();
+        lat_str = String(latitude, 6);
+        longitude = gps.location.lng();
+        lng_str = String(longitude, 6);
+        latarray[i] = lat_str;
+        lngarray[i] = lng_str;
+        i++;
+        Serial.print(i);
+        if (i >= 20)
+          // {
+          i = 0; //reset to beginning of array, so you don't try to save readings outside of the bounds of the array
+        // }
+        Serial.println("Latitude:");
+        Serial.println(latarray[i]);
+        Serial.println("Longitude:");
+        Serial.println(lngarray[i]);
+        Serial.println();
+        display.setCursor(0, 20);
+        display.println("Lat:");
+        display.setCursor(27, 20);
+        display.println(lat_str);
+        display.setCursor(0, 40);
+        display.println("Lng:");
+        display.setCursor(27, 40);
+        display.println(lng_str);
+        display.display();
+        postdata(latitude, longitude);
+      }
+      previousTime = currentTime;
+    }
+    //}
+
+  }
+}
+void postdata(float lat1, float lng1) {
+  if (WiFi.status() != WL_CONNECTED) return;
+  WiFiClientSecure client;
+  HTTPClient http;
+  client.setInsecure();
+  // Your Domain name with URL path or IP address with path
+  http.begin(client, serverName);
+
+
+  // If you need an HTTP request with a content type: application/json, use the following:
+  http.addHeader("Content-Type", "application/json");
+  doc["lat"] = lat1;
+  doc["lng"] = lng1;
+  doc["direction"] = "up";
+  serializeJson(doc, output);
+
+  int httpResponseCode = http.POST(output);
+
+  // If you need an HTTP request with a content type: text/plain
+  //http.addHeader("Content-Type", "text/plain");
+  //int httpResponseCode = http.POST("Hello, World!");
+
+  Serial.print("HTTP Response code: ");
+  Serial.println(httpResponseCode);
+
+  // Free resources
+  http.end();
+
+}
+
+void loop() {
+
+  display.clearDisplay();
+  if (WiFi.status() == WL_CONNECTED) {
+    displayPrint("WiFi : Connected", 2);
+  } else {
+    displayPrint("WiFi Disconnected!", 2);
+    displayPrint("== Turn on WiFi ==" , 3);
+    displayPrint("SSID:" + String(ssid), 4);
+    displayPrint("PWD:" + String(password) , 5);
+    return ;
+  }
+
+
+  // get configurations from api.
+  while (ss.available() > 0)
+    checkandreadGPS();
+  // post cordinated to api.
+  // check wifi connection.
+}
